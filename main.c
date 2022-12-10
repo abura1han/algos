80118d70a10dca9da423bbac5f18c008ec1d1be0
abura1han <xbura1han@gmail.com>
2022-12-10 10:38:25 +0600

    Bubble sort algo

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "bubbleSort.c"
+
+int main() {
+  int myArray[] = {2,1,33,343,52,1,54,3};
+  int size = sizeof(myArray) / sizeof(myArray[0]);
+
+  bubbleSort(myArray, size);
+
+  for (int i = 0; i < size - 1; i++) {
+    printf("%d, ", myArray[i]);
+  }
+  
+
+  return 0;
+}
