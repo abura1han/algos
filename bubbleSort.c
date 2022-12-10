f1d7231bc267b62b64c43d10c5442c7f11eee0a8
abura1han <xbura1han@gmail.com>
2022-12-10 12:01:05 +0600

    Fix: not printing all elements of array

--- bubbleSort.c
+++ bubbleSort.c
@@ -2,17 +2,15 @@
 
 void bubbleSort(int array[], int size, char order[])
 {
-    int i, j, temp;
-
-    for (i = 0; i < size - 1; i++)
+    for (int i = 0; i < size - 1; i++)
     {
-        for (j = 0; j < size - i - 1; j++)
+        for (int j = 0; j < size - i - 1; j++)
         {
             if (order == "des")
             {
                 if (array[j] < array[j + 1])
                 {
-                    temp = array[j];
+                    int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
@@ -21,7 +19,7 @@ void bubbleSort(int array[], int size, char order[])
             {
                 if (array[j] > array[j + 1])
                 {
-                    temp = array[j];
+                    int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "bubbleSort.c"
 
-int main() {
-  int myArray[] = {2,1,33,343,52,1,54,3};
+int main()
+{
+  int myArray[] = {2, 33, 343, 52, 1, 54, 3};
   int size = sizeof(myArray) / sizeof(myArray[0]);
 
   bubbleSort(myArray, size, "des");
 
-  for (int i = 0; i < size - 1; i++) {
+  for (int i = 0; i < size; i++)
+  {
     printf("%d, ", myArray[i]);
   }
-  
 
   return 0;
 }
